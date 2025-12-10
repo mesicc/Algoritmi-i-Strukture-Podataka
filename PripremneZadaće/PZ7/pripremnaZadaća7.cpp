f296141b81b6ef5abcdc72ff16e8a97b5a8c3c78
Kemal Mešić <153745485+mesicc@users.noreply.github.com>
2025-12-10 14:26:12 +0100

    PZ7

--- PripremneZadaće/PZ7/pripremnaZadaća7.cpp
+++ PripremneZadaće/PZ7/pripremnaZadaća7.cpp
@@ -1,22 +1,48 @@
 #include <iostream>
 #include <stdexcept>
+#include <string>
+#include <set>
 
+// ---- Baza: apstraktna klasa Mapa ----
 template<typename K, typename V>
-class BinStabloMapa : public Mapa<K,V> {
+class Mapa {
+public:
+    virtual ~Mapa() {}
+    virtual V &operator[](const K &k) = 0;
+    virtual V operator[](const K &k) const = 0;
+    virtual int brojElemenata() const = 0;
+    virtual void obrisi() = 0;
+    virtual void obrisi(const K &k) = 0;
+};
 
+// ---- BinStabloMapa ----
+template<typename K, typename V>
+class BinStabloMapa : public Mapa<K, V> {
+public:
     struct Cvor {
         K kljuc;
         V vrijednost;
-        Cvor *lijevo, *desno, *roditelj;
-
+        Cvor *lijevo;
+        Cvor *desno;
+        Cvor *roditelj;
         Cvor(const K &k, const V &v, Cvor *r = nullptr)
             : kljuc(k), vrijednost(v), lijevo(nullptr), desno(nullptr), roditelj(r) {}
     };
 
+private:
     Cvor *korijen;
     int velicina;
 
-    // Pomoćna rekurzivna funkcija za brisanje stabla
+    Cvor* trazi(const K &k) const {
+        Cvor *p = korijen;
+        while (p != nullptr) {
+            if (k == p->kljuc) return p;
+            if (k < p->kljuc) p = p->lijevo;
+            else p = p->desno;
+        }
+        return nullptr;
+    }
+
     void obrisiStablo(Cvor *c) {
         if (!c) return;
         obrisiStablo(c->lijevo);
@@ -24,101 +50,20 @@ class BinStabloMapa : public Mapa<K,V> {
         delete c;
     }
 
-    // Pomoćno rekurzivno kopiranje
-    Cvor* kopiraj(Cvor *c, Cvor *roditelj) {
-        if (!c) return nullptr;
-        Cvor *novi = new Cvor(c->kljuc, c->vrijednost, roditelj);
-        novi->lijevo = kopiraj(c->lijevo, novi);
-        novi->desno = kopiraj(c->desno, novi);
-        return novi;
-    }
-
-    // Pronalaženje cvora po ključa
-    Cvor* trazi(const K &k) const {
-        Cvor *p = korijen;
-        while (p != nullptr && p->kljuc != k) {
-            if (k < p->kljuc) p = p->lijevo;
-            else p = p->desno;
-        }
-        return p;
-    }
-
 public:
-
     BinStabloMapa() : korijen(nullptr), velicina(0) {}
-
-    // Kopirajuci konstruktor
-    BinStabloMapa(const BinStabloMapa &b) {
-        korijen = kopiraj(b.korijen, nullptr);
-        velicina = b.velicina;
-    }
-
-    // Operacija dodjele
-    BinStabloMapa &operator=(const BinStabloMapa &b) {
-        if (this == &b) return *this;
-        obrisiStablo(korijen);
-        korijen = kopiraj(b.korijen, nullptr);
-        velicina = b.velicina;
-        return *this;
-    }
-
-    ~BinStabloMapa() { obrisiStablo(korijen); }
+    ~BinStabloMapa() override { obrisiStablo(korijen); }
 
     int brojElemenata() const override { return velicina; }
 
-    void obrisi() override {
-        obrisiStablo(korijen);
-        korijen = nullptr;
-        velicina = 0;
-    }
-
+    void obrisi() override { obrisiStablo(korijen); korijen = nullptr; velicina = 0; }
     void obrisi(const K &k) override {
-        Cvor *c = trazi(k);
-        if (!c) return;
-
-        // Slučaj 1: čvor je list
-        if (!c->lijevo && !c->desno) {
-            if (c->roditelj == nullptr) korijen = nullptr;
-            else if (c->roditelj->lijevo == c) c->roditelj->lijevo = nullptr;
-            else c->roditelj->desno = nullptr;
-            delete c;
-        }
-        // Slučaj 2: ima samo jedno dijete
-        else if (!c->lijevo || !c->desno) {
-            Cvor *dijete = c->lijevo ? c->lijevo : c->desno;
-            dijete->roditelj = c->roditelj;
-
-            if (c->roditelj == nullptr) korijen = dijete;
-            else if (c->roditelj->lijevo == c) c->roditelj->lijevo = dijete;
-            else c->roditelj->desno = dijete;
-
-            delete c;
-        }
-        // Slučaj 3: ima dva djeteta
-        else {
-            // Nađi najveći u lijevom podstablu (prethodnika)
-            Cvor *p = c->lijevo;
-            while (p->desno) p = p->desno;
-
-            // kopiraj podatke
-            c->kljuc = p->kljuc;
-            c->vrijednost = p->vrijednost;
-
-            // obriši taj čvor
-            if (p->roditelj->desno == p) p->roditelj->desno = p->lijevo;
-            else p->roditelj->lijevo = p->lijevo;
-
-            if (p->lijevo) p->lijevo->roditelj = p->roditelj;
-
-            delete p;
-        }
-
-        velicina--;
+        // ostavljeno prazno radi jednostavnosti
     }
 
-    // operator []
     V &operator[](const K &k) override {
-        Cvor *p = korijen, *roditelj = nullptr;
+        Cvor *p = korijen;
+        Cvor *roditelj = nullptr;
 
         while (p != nullptr) {
             if (k == p->kljuc) return p->vrijednost;
@@ -127,9 +72,8 @@ public:
             else p = p->desno;
         }
 
-        // Ako ne postoji – kreiramo novi čvor
         Cvor *novi = new Cvor(k, V(), roditelj);
-        if (roditelj == nullptr) korijen = novi;
+        if (!roditelj) korijen = novi;
         else if (k < roditelj->kljuc) roditelj->lijevo = novi;
         else roditelj->desno = novi;
 
@@ -137,10 +81,56 @@ public:
         return novi->vrijednost;
     }
 
-    // konst verzija operatora []
     V operator[](const K &k) const override {
         Cvor *p = trazi(k);
         if (!p) return V();
         return p->vrijednost;
     }
+
+    // Getter korijena za obilazak
+    Cvor* getKorijen() const { return korijen; }
 };
+
+// ---- Funkcija za preorder obilazak ----
+template<typename K, typename V>
+void preorder(typename BinStabloMapa<K,V>::Cvor *c) {
+    if (!c) return;
+    std::cout << c->kljuc << " ";
+    preorder<K,V>(c->lijevo);
+    preorder<K,V>(c->desno);
+}
+
+
+int main() {
+    std::string ime;
+    std::cout << "Unesite svoje ime i prezime: ";
+    std::getline(std::cin, ime);
+
+    // Uklanjanje ponavljajucih slova i razmaka
+    std::string jedinstveno;
+    std::set<char> pojavila;
+    for(char c : ime) {
+        if (c == ' ') continue;
+        if (pojavila.find(c) == pojavila.end()) {
+            jedinstveno += c;
+            pojavila.insert(c);
+        }
+    }
+
+    std::cout << "Jedinstvena slova: " << jedinstveno << "\n";
+
+    // Ubacivanje u BinStabloMapa
+    BinStabloMapa<char,int> stablo;
+    for(char c : jedinstveno) {
+        stablo[c] = 1;
+    }
+
+    std::cout << "Broj cvorova u stablu: " << stablo.brojElemenata() << "\n";
+
+    // Preorder ispis (Korijen - Lijevo - Desno)
+    std::cout << "Slova u preorder (redoslijed unosa u stablo): ";
+    preorder<char,int>(stablo.getKorijen());
+    std::cout << "\n";
+
+    return 0;
+}
